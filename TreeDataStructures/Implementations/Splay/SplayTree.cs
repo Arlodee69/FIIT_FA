afd9eedcbe20b981ed345e1797048e0370e37434
Anton <antonandrianov69@gmail.com>
2026-03-28 12:48:41 +0300

    3 and 4

--- TreeDataStructures/Implementations/Splay/SplayTree.cs
+++ TreeDataStructures/Implementations/Splay/SplayTree.cs
@@ -6,22 +6,97 @@ namespace TreeDataStructures.Implementations.Splay;
 public class SplayTree<TKey, TValue> : BinarySearchTree<TKey, TValue>
     where TKey : IComparable<TKey>
 {
-    protected override BstNode<TKey, TValue> CreateNode(TKey key, TValue value)
-        => new(key, value);
-    
     protected override void OnNodeAdded(BstNode<TKey, TValue> newNode)
     {
-        throw new NotImplementedException();
+        Splay(newNode);
     }
     
     protected override void OnNodeRemoved(BstNode<TKey, TValue>? parent, BstNode<TKey, TValue>? child)
     {
-        throw new NotImplementedException();
+        if (parent != null)
+        {
+            Splay(parent);
+        }
     }
-    
+
     public override bool TryGetValue(TKey key, [MaybeNullWhen(false)] out TValue value)
     {
-        throw new NotImplementedException();
+        BstNode<TKey, TValue>? current = Root;
+        BstNode<TKey, TValue>? lastAccessed = null;
+
+        while (current != null)
+        {
+            int cmp = Comparer.Compare(key, current.Key);
+
+            if (cmp == 0)
+            {
+                Splay(current);
+                value = current.Value;
+                return true;
+            }
+
+            lastAccessed = current;
+            current = cmp < 0 ? current.Left : current.Right;
+        }
+
+        if (lastAccessed != null)
+        {
+            Splay(lastAccessed);
+        }
+
+        value = default;
+        return false;
     }
-    
+
+    public override bool ContainsKey(TKey key)
+    {
+        return TryGetValue(key, out _);
+    }
+
+    protected void Splay(BstNode<TKey, TValue> x)
+    {
+        while (x.Parent != null)
+        {
+            BstNode<TKey, TValue> p = x.Parent;
+            BstNode<TKey, TValue>? g = p.Parent;
+
+            if (g == null)
+            {
+                // Zig:У узла только родитель корень
+                if (x.IsLeftChild) RotateRight(p);
+                else RotateLeft(p);
+            }
+            else
+            {
+                if (x.IsLeftChild == p.IsLeftChild)
+                {
+                    // Zig-Zig:x и p оба левые или правые дети
+                    if (x.IsLeftChild)
+                    {
+                        RotateRight(g);
+                        RotateRight(p);
+                    }
+                    else
+                    {
+                        RotateLeft(g);
+                        RotateLeft(p);
+                    }
+                }
+                else
+                {
+                    // Zig-Zag:x левый ребенок, p правый (или наоборот)
+                    if (x.IsLeftChild)
+                    {
+                        RotateRight(p);
+                        RotateLeft(g);
+                    }
+                    else
+                    {
+                        RotateLeft(p);
+                        RotateRight(g);
+                    }
+                }
+            }
+        }
+    }
 }
